21012f40f9906f8401763a9b083182556f55b467
strake <strake888@gmail.com>
2012-12-13 17:21:01 -0500

    add config.h

---
+++ config.h
@@ -0,0 +1,81 @@
+/* Debug messages. */
+/* #undef ENABLE_DEBUG */
+
+/* System logging. */
+#define ENABLE_LOGGING 1
+
+/* Enable Xz for modules. */
+#define ENABLE_XZ 1
+
+/* Enable zlib for modules. */
+#define ENABLE_ZLIB 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if `st_mtim' is a member of `struct stat'. */
+#define HAVE_STRUCT_STAT_ST_MTIM 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if typeof works with your compiler. */
+#define HAVE_TYPEOF 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `__xstat' function. */
+#define HAVE___XSTAT 1
+
+/* Name of package */
+#define PACKAGE "kmod"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "linux-modules@vger.kernel.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "kmod"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "kmod 9"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "kmod"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://git.kernel.org/?p=utils/kernel/kmod/kmod.git"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "9"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "9"
+
+#ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
